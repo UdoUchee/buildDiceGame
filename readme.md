fc6ffcabbc61e79d12b16424c2122cd563a4bf7f
Udochukwu Uche <81819606+UdoUchee@users.noreply.github.com>
2022-01-04 06:01:14 +0000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,42 @@
+# *#buildJs*
+# Building a simple Dice Game
+*2022-01-03*
+
+![Two dice rolling 6's!](/dice.jpg "Willi Heidelbach")
+
+**This simple Dice game allows two playes to keep rolling the dice until a player gets a total of 20 points and is declared the winner.**
+
+
+**#CodeSteps**
+
+1. Create variables for the game state
+1. Create variables to store references to the necessary DOM nodes
+1. Hook up a click event listener to the Roll Dice Button. Log out a random
+ number between 1 and 6. 
+ > Hint: use Math.floor() and Math.random()
+1. Find out which players turn it is
+1. log out the value e.g. **"Player 1 rolled 5"**
+1. Switch the turn back to the other player
+1. Display the dice number instead of logging it out
+1. Use the 'active' class to show which player's turn it is 
+    > * Hint: use the .classList.remove() and classlist.add() methods*
+1. Update the "message" DOM node so that it states who's turn it is
+1. Update the scores for each player
+1. Display the scores in their scoreboards
+
+
+
+> For more check : 
+ -  [Math method](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math)
+ -  [ClassList method](https://developer.mozilla.org/en-US/docs/Web/API/Element/classList)
+
+## Languages used
+	- HTML
+	- CSS
+	- JavaScript
+## Prerequisties
+IDE
+
+## Credits
+[Per Harald Borgen](https://scrimba.com/allcourses)
+
---
+++ script.js
@@ -0,0 +1,45 @@
+// 1. Create variables for the game state
+let player1Score = 0
+let player2Score = 0
+let player1Turn = true
+
+// 2. Create variables to store references to the necessary DOM nodes
+const messageEl = document.getElementById("message")
+const player1ScoreboardEl = document.getElementById("player1Scoreboard")
+const player2ScoreboardEl = document.getElementById("player2Scoreboard")
+const player1DiceEl = document.getElementById("player1Dice")
+const player2DiceEl = document.getElementById("player2Dice")
+const rollBtnEl = document.getElementById("rollBtn")
+const resetBtnEl = document.getElementById("resetBtn")
+
+/* 3. Hook up a click event listener to the Roll Dice Button. Log out a random
+ number between 1 and 6. Hint: use Math.floor() and Math.random() */
+
+
+rollBtnEl.addEventListener("click", function () {
+  const randomNumber = Math.floor(Math.random() * 6) + 1
+  console.log(randomNumber)
+  
+// 4. Find out which players turn it is
+// 5. log out the value e.g. "Player 1 rolled 5" > //  console.log("Player-1 rolled " + randomNumber)
+  // Hint: use the .classList.remove() and classList.add() methods
+  if (player1Turn) {
+    player1Score += randomNumber // 10. Update the scores for each player
+    player1ScoreboardEl.textContent = player1Score // 11. Display the scores in their scoreboards
+    player1DiceEl.textContent = randomNumber // 7. Display the dice number instead of logging it out
+    player1DiceEl.classList.remove("active") // 8. Use the 'active' class to show which player's turn it is 
+    player2DiceEl.classList.add("active")
+    messageEl.textContent = "Player 2 Turn" // 9. Update the "message" DOM node so that it states who's turn it is
+  } else {
+    player2Score += randomNumber
+    player2ScoreboardEl.textContent = player2Score
+    player2DiceEl.textContent = randomNumber
+    player2DiceEl.classList.remove("active")
+    player1DiceEl.classList.add("active")
+    messageEl.textContent = "Player 1 Turn"
+  }
+  player1Turn = !player1Turn // 6. Switch the turn back to the other player > ! = opposite of the value to the left of the "!"
+})
+
+
+
